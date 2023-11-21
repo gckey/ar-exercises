c88c86c32e4bdc1f8cd803fb49a47a290b1a7e75
Abdurahman Mohammed <abdorehman@gmail.com>
2023-11-20 20:42:33 -0600

    Add validation

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,17 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+# Ask the user for a store name
+puts "Enter a store name:"
+store_name = gets.chomp
+
+# Attempt to create a store with the inputted name but leave out other fields
+new_store = Store.new(name: store_name)
+
+# Print the error messages provided back from ActiveRecord
+if new_store.save
+  puts "Store created successfully!"
+else
+  puts "Failed to create store. Errors:"
+  puts new_store.errors.full_messages
+end
