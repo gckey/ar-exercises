e38deaf0d5c587bab4534ed5a2d62c730dba69d1
Abdurahman Mohammed <abdorehman@gmail.com>
2023-11-20 20:12:01 -0600

    create one-to-many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,35 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+# Add some data into employees under @store1 and @store2
+@store1.employees.create(
+  first_name: "Khurram",
+  last_name: "Virani",
+  hourly_rate: 60
+)
+
+@store1.employees.create(
+  first_name: "John",
+  last_name: "Doe",
+  hourly_rate: 50
+)
+@store1.employees.create(
+  first_name: "Jane",
+  last_name: "Smith",
+  hourly_rate: 55
+)
+
+@store2.employees.create(
+  first_name: "Bob",
+  last_name: "Johnson",
+  hourly_rate: 45
+)
+@store2.employees.create(
+  first_name: "Alice",
+  last_name: "Williams",
+  hourly_rate: 48
+)
+
+# Print the number of employees for store 1 and 2
+puts "Number of employees at #{@store1.name}: #{@store1.employees.count}"
+puts "Number of employees at #{@store2.name}: #{@store2.employees.count}"
